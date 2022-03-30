82cabadaa7af53a33768750a50cd74bf3d3d5d36
Muhammad Alkady <kady.muhammad@gmail.com>
2022-03-30 15:46:29 +0200

    Add TopAppBar

--- app/src/main/java/kady/muhammad/paytabstask/presentation/main/MainActivity.kt
+++ app/src/main/java/kady/muhammad/paytabstask/presentation/main/MainActivity.kt
@@ -3,32 +3,64 @@ package kady.muhammad.paytabstask.presentation.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import kady.muhammad.paytabstask.R
 import kady.muhammad.paytabstask.presentation.characters_screen.CharactersScreen
+import kady.muhammad.paytabstask.presentation.ui.theme.Black
 import kady.muhammad.paytabstask.presentation.ui.theme.PaytabsTaskTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, true)
         setContent {
+            val systemUiController = rememberSystemUiController()
+            SideEffect {
+                // Update all of the system bar colors to be transparent, and use
+                // dark icons if we're in light theme
+                systemUiController.setSystemBarsColor(
+                    color = Black,
+                    darkIcons = false
+                )
+            }
             PaytabsTaskTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    CharactersScreen()
+                    Scaffold(topBar = { AppTopBar() }) {
+                        CharactersScreen()
+                    }
                 }
             }
         }
     }
+
 }
 
+@Composable
+private fun AppTopBar() {
+    TopAppBar {
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            painter = painterResource(id = R.drawable.ic_marvel_logo),
+            contentDescription = null, alignment = Alignment.Center
+        )
+    }
+
+}
 
 @Preview(showBackground = true)
 @Composable
--- app/src/main/java/kady/muhammad/paytabstask/presentation/ui/theme/Color.kt
+++ app/src/main/java/kady/muhammad/paytabstask/presentation/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package kady.muhammad.paytabstask.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Black700 = Color(0xFF616161)
+val Black900 = Color(0xFF212121)
+val Black = Color(0xFF000000)
+val Orange900 = Color(0xFFE65100)
